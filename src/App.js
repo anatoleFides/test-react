d40f7385fe4139d000c2a9a2a216d72e938568cc
Anatolii Bedilo <fides2013ml@gmail.com>
2022-05-08 23:00:33 +0300

    added component postList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import ClassCounter from './components/ClassCounter';
+import React, { useState } from 'react';
+
+import PostList from './components/PostList';
+
+import './styles/App.css';
+
 
 function App() {
 
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript 1', body: 'Description' },
+    { id: 2, title: 'Javascript 2', body: 'Description' },
+    { id: 3, title: 'Javascript 3', body: 'Description' }
+  ])
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Pyhton 1', body: 'Description' },
+    { id: 2, title: 'Pyhton 2', body: 'Description' },
+    { id: 3, title: 'Pyhton 3', body: 'Description' }
+  ])
+
   return (
-    <div className="App">
-      <ClassCounter />
+    <div className='App'>
+      <PostList posts={posts} title={'Посты про JS'} />
+      <PostList posts={posts2} title={'Посты про Pyhton'} />
 
     </div >
+
   );
 }
 
