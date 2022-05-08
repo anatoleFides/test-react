036a2c2b9b2f18c903999a0417354799f8bde542
Anatolii Bedilo <fides2013ml@gmail.com>
2022-05-08 20:55:20 +0300

    управляемое связывание

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+
+  const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('Hello!')
+
+  function increment() {
+    setLikes(likes + 1)
+  }
+
+  function decrement() {
+    setLikes(likes - 1)
+  }
+
   return (
     <div className="App">
-
-    </div>
+      <h1>{likes}</h1>
+      <h2>{value}</h2>
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <div>Старт React</div>
-    <button>Кнопка</button>
+    <App />
   </div>
 );
